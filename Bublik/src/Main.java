c555d32fb5d00e401a54573a16577d06eba78aa4
Gleb <125480807+nemnogobread@users.noreply.github.com>
2024-02-13 19:24:34 +0300

    incorrect circle created

---
+++ Bublik/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class Main {
+    public static String[] formation(char[][] array){
+        String[] result = new String[30];
+        for (int i = 0; i < 30; i++){
+            String formattedString = Arrays.toString(array[i])
+                    .replace(",", "")  //remove the commas
+                    .replace("[", "")  //remove the right bracket
+                    .replace("]", "")  //remove the left bracket
+                    .replace(" ", "")
+                    .trim();
+            result[i] = formattedString;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        char[][] charArray = new char [30][120];
+        for (int i = 0; i < 30; i++){
+            for (int j = 0; j < 120; j++) {
+                if (Math.pow(i-15, 2) + Math.pow(j-60 ,2) <= 100){
+                    charArray[i][j] = '@' ;
+                }
+                else {
+                    charArray[i][j] = '.' ;
+                }
+            }
+        }
+
+        String[] consolResult = formation(charArray);
+
+        for (int i = 0; i < 30; i++){
+            System.out.println(String.format("%s", consolResult[i]));
+        }
+    }
+}
