3d37bb31d432b57026a4d157d61f30d6da1ccdb6
Gleb <glebmiloslavov@gmail.com>
2024-03-29 16:25:58 +0300

    MVC trying

--- spring-mvc-example/src/main/java/org/example/HelloController.java
+++ spring-mvc-example/src/main/java/org/example/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("/spring-mvc-example/hello-world")
+    @GetMapping(value = "/hello-world")
     public String sayHello(){
         return "hello_world";
     }
