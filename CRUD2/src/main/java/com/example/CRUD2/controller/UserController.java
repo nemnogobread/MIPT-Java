fcf52f4c2833509449a949523712e57920e18f5a
Gleb <glebmiloslavov@gmail.com>
2024-04-03 16:01:57 +0300

    added create, update, delete,

--- CRUD2/src/main/java/com/example/CRUD2/controller/UserController.java
+++ CRUD2/src/main/java/com/example/CRUD2/controller/UserController.java
@@ -4,8 +4,11 @@ import com.example.CRUD2.model.User;
 import com.example.CRUD2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -19,5 +22,35 @@ public class UserController {
         List<User> users = userService.findAll();
         model.addAttribute("users", users);
         return "user-list";
+    }
+
+    @GetMapping("/user-create")
+    public String createUserForm(User user){
+        return "user-create";
+    }
+
+    @PostMapping("/user-create")
+    public String createUser(User user){
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/user-delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id){
+        userService.deleteById(id);
+        return "redirect:/users";
+    }
+
+    @GetMapping("user-update/{id}")
+    public String updateUserForm(@PathVariable("id") Long id, Model model){
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+        return "user-update";
+    }
+
+    @PostMapping("user-update")
+    public String updateUser(User user){
+        userService.saveUser(user);
+        return "redirect:/users";
     }
 }
