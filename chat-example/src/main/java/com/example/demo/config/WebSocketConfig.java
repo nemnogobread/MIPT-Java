12a256d7cb8b5ca98d1314250e12158adbd25b5c
Gleb <125480807+nemnogobread@users.noreply.github.com>
2024-04-12 01:36:49 +0300

    added hamachi ip in allowed origins

--- chat-example/src/main/java/com/example/demo/config/WebSocketConfig.java
+++ chat-example/src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("https://localhost:8080").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("https://localhost:8080", "http://25.66.180.0:3000", "http://localhost:3000/").withSockJS();
     }
 
     @Override
